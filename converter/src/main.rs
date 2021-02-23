901902d43749f568f1103fde1a03d8217d50628d
Arian Hanifi <counterstrike1337@hotmail.com>
2021-02-23 20:25:33 +0100

    add magic file generator

--- converter/src/main.rs
+++ converter/src/main.rs
@@ -1,4 +1,4 @@
-use std::error::Error;
+use std::{error::Error, io::Write, str::Chars, usize};
 use std::fs::File;
 use std::io::{BufReader, BufRead, Read};
 
@@ -6,7 +6,8 @@ use encoding_rs::WINDOWS_1252;
 use encoding_rs_io::DecodeReaderBytesBuilder;
 
 fn main(){
-    create_index_file("../token.txt".to_string());
+    create_magic_file("index_file.txt".to_string());
+    //create_index_file("../token.txt".to_string());
 }
 
 fn create_magic_file(path:String){
@@ -16,7 +17,41 @@ fn create_magic_file(path:String){
             .encoding(Some(WINDOWS_1252))
             .build(file));
 
-    
+    const permutations:usize=30*30*30;
+    let mut indicies=[-1;permutations];
+    for (i,l )in reader.lines().enumerate(){
+        let line = l.unwrap();
+        if line.len()==0{
+            continue;
+        }
+        let mut words=line.split_whitespace();
+        let index_string=format!("{:`<3}",words.next().unwrap());
+        let mut chars=index_string.chars();
+        let index=calculate_index(&mut chars);
+        indicies[index]=i as isize;
+    }
+    let mut f = File::create("magic_johnson.txt").expect("Unable to create file");                                                                                                          
+    for i in &indicies{
+        f.write_all(&[b'\n']);                                                                                                                                                                  
+        f.write_all((i).to_string().as_bytes()).expect("Unable to write data");                                                                                                                            
+    }  
+
+    //let index= a as usize -97 + (b as usize -97)*30 + (c as usize -97)*30*30;
+}
+
+fn calculate_index(chars:&mut Chars) -> usize{
+    let mut index=0;
+    for i in (0..3).rev(){
+        let char_val=match chars.next().unwrap() {
+            'ö' => 29,
+            'ä' => 28,
+            'å' => 27,
+            n=> n as usize -96,
+
+        };
+        index+=30usize.pow(i)*char_val
+    }
+    index
 }
 
 fn create_index_file(path:String){
@@ -31,7 +66,7 @@ fn create_index_file(path:String){
     let mut current_index=String::new();
     
     for l in reader.lines() {
-        let line=l.unwrap().clone();
+        let line=l.unwrap();
         if line.len()==0{
             break;
         }
@@ -53,3 +88,5 @@ fn create_index_file(path:String){
     let encoded =WINDOWS_1252.encode(&working_string).0;
     std::fs::write("index_file.txt", encoded);
 }
+
+// format!("{:0>3b}", index_registry);
