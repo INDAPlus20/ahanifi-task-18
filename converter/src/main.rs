6eb8e8236a2fd61f6053a2b042274945f6a3fe68
Arian Hanifi <counterstrike1337@hotmail.com>
2021-02-24 11:43:53 +0100

    add instant lookup functionality

--- converter/src/main.rs
+++ converter/src/main.rs
@@ -1,92 +1,134 @@
+use core::panic;
+use std::{cmp::min, fs::File};
+use std::io::{BufRead, BufReader, Read};
 use std::{error::Error, io::Write, str::Chars, usize};
-use std::fs::File;
-use std::io::{BufReader, BufRead, Read};
 
 use encoding_rs::WINDOWS_1252;
 use encoding_rs_io::DecodeReaderBytesBuilder;
 
-fn main(){
-    create_magic_file("index_file.txt".to_string());
+fn main() {
+    create_magic_file("../index_file.txt".to_string());
     //create_index_file("../token.txt".to_string());
 }
 
-fn create_magic_file(path:String){
-    let file = File::open(path).unwrap();
-    let mut reader = BufReader::new(
-        DecodeReaderBytesBuilder::new()
-            .encoding(Some(WINDOWS_1252))
-            .build(file));
+fn create_magic_file(path: String) {
+    let mut file = File::open(path).unwrap();
+    // let mut reader = BufReader::new(
+    //     DecodeReaderBytesBuilder::new()
+    //         .encoding(Some(WINDOWS_1252))
+    //         .build(file),
+    // );
+    let mut buf = Vec::new();
+    file.read_to_end(&mut buf);
 
-    const permutations:usize=30*30*30;
-    let mut indicies=[-1;permutations];
-    for (i,l )in reader.lines().enumerate(){
-        let line = l.unwrap();
-        if line.len()==0{
-            continue;
+    const permutations: usize = 30 * 30 * 30;
+    let mut indicies = [-1; permutations];
+
+    let mut word = Vec::<u8>::new();
+    let mut current_word:Vec<u8>=vec![];
+    let mut is_word = true;
+    
+    let mut prefix:Vec<u8>=vec![];
+
+    for (i, b) in buf.iter().enumerate() {
+        match b {
+            b' ' => {
+                if is_word && prefix != current_word {
+                    let index = calculate_index_from_byte(&prefix);
+                    indicies[index-1] = (i  - prefix.len()) as isize;
+                    // println!("{}", std::str::from_utf8(&prefix).unwrap());
+                    // println!("{}",index);
+                    // panic!();
+                }
+                is_word = false;
+            }
+            b'\n' => {
+                current_word=prefix.clone();
+                prefix.clear();
+                is_word = true;
+            }
+            byte => {
+                if is_word && prefix.len()<3{
+                    prefix.push(*byte);
+                }
+            }
         }
-        let mut words=line.split_whitespace();
-        let index_string=format!("{:`<3}",words.next().unwrap());
-        let mut chars=index_string.chars();
-        let index=calculate_index(&mut chars);
-        indicies[index]=i as isize;
     }
-    let mut f = File::create("magic_johnson.txt").expect("Unable to create file");                                                                                                          
-    for i in &indicies{
-        f.write_all(&[b'\n']);                                                                                                                                                                  
-        f.write_all((i).to_string().as_bytes()).expect("Unable to write data");                                                                                                                            
-    }  
+
+    let mut f = File::create("../magic_johnson.txt").expect("Unable to create file");
+    for i in &indicies {
+        f.write_all((i).to_string().as_bytes())
+            .expect("Unable to write data");
+        f.write_all(&[b'\n']);
+    }
+    f.flush();
 
     //let index= a as usize -97 + (b as usize -97)*30 + (c as usize -97)*30*30;
 }
 
-fn calculate_index(chars:&mut Chars) -> usize{
-    let mut index=0;
-    for i in (0..3).rev(){
-        let char_val=match chars.next().unwrap() {
-            'ö' => 29,
-            'ä' => 28,
-            'å' => 27,
-            n=> n as usize -96,
-
+fn calculate_index_from_byte(bytes: &Vec<u8>) -> usize {
+    let mut index = 0;
+    let mut iter = bytes.iter();
+    for i in (0..3).rev() {
+        let char_val = match iter.next() {
+            Some(228) => {28},
+            Some(229) => {27},
+            Some(246) => {29},
+            Some(n) => n - 96,
+            None => 0,
         };
-        index+=30usize.pow(i)*char_val
+        index += 30usize.pow(i) * char_val as usize
     }
     index
 }
 
-fn create_index_file(path:String){
+fn calculate_index(chars: &mut Chars) -> usize {
+    let mut index = 0;
+    for i in (0..3).rev() {
+        let char_val = match chars.next().unwrap() {
+            'ö' => 29,
+            'ä' => 28,
+            'å' => 27,
+            n => n as usize - 96,
+        };
+        index += 30usize.pow(i) * char_val
+    }
+    index
+}
 
+fn create_index_file(path: String) {
     let file = File::open(path).unwrap();
     let mut reader = BufReader::new(
         DecodeReaderBytesBuilder::new()
             .encoding(Some(WINDOWS_1252))
-            .build(file));
+            .build(file),
+    );
 
-    let mut working_string=String::new();
-    let mut current_index=String::new();
-    
+    let mut working_string = String::new();
+    let mut current_index = String::new();
+
     for l in reader.lines() {
-        let line=l.unwrap();
-        if line.len()==0{
+        let line = l.unwrap();
+        if line.len() == 0 {
             break;
         }
 
-        let mut split_line=line.trim().split_ascii_whitespace();
-        let index= split_line.next().unwrap();
+        let mut split_line = line.trim().split_ascii_whitespace();
+        let index = split_line.next().unwrap();
 
-        if index != current_index{
+        if index != current_index {
             working_string.push_str("\n");
             working_string.push_str(index);
-            current_index=index.to_string();
+            current_index = index.to_string();
         }
-        for value in split_line{
+        for value in split_line {
             working_string.push(' ');
             working_string.push_str(value);
-            
         }
     }
-    let encoded =WINDOWS_1252.encode(&working_string).0;
-    std::fs::write("index_file.txt", encoded);
+    working_string.remove(0);
+    let encoded = WINDOWS_1252.encode(&working_string).0;
+    std::fs::write("../index_file.txt", encoded);
 }
 
 // format!("{:0>3b}", index_registry);
---
+++ finder/src/main.rs
@@ -0,0 +1,103 @@
+use std::{cmp::{max, min}, env, fs, io::{Read, Seek, SeekFrom}, str::{Chars, from_utf8}, usize};
+use encoding_rs::WINDOWS_1252;
+use fs::{File, read};
+
+fn main() {
+    let index_path="../index_file.txt";
+    let magic_path="../magic_johnson.txt";
+
+    let mut index_file=File::open(index_path).unwrap();
+
+    let magic_file=fs::read_to_string(magic_path).unwrap();
+    let magic_lines:Vec<&str>=magic_file.split("\n").collect();
+
+    let mut korpus_file=File::open("../korpus").unwrap();
+    let korpus_size=korpus_file.metadata().unwrap().len();
+
+    let margin=40;
+
+    let args: Vec<String> = env::args().collect();
+    let input;
+    if let Some(s)= args.get(1){
+        input=s;
+    }else{
+        println!("Please specify which word you want to look up");
+        return;
+    }
+
+    let magic_index=calculate_index(&mut input.chars());
+    let i=magic_lines[magic_index-1].parse::<isize>().unwrap();
+
+    if i == -1{
+        println!("Word doesn't exist");
+        return;
+    }
+    
+    let mut cap;
+    if magic_index+1 != magic_lines.len(){
+        cap=korpus_size;
+        for j in magic_index+1..magic_lines.len(){
+            let val=magic_lines[j].parse::<isize>().unwrap();
+            if val != -1{
+                cap=val as u64;
+                break;
+            }
+        }
+    }else{
+        cap=korpus_size;
+    }
+
+    index_file.seek(SeekFrom::Start(i as u64)).unwrap();
+
+    let mut buf=Vec::<u8>::new();
+    index_file.take(cap-i as u64).read_to_end(&mut buf).unwrap();
+    let decoded=WINDOWS_1252.decode(&buf).0;
+    let string=decoded.split("\n");
+
+    for line in string{
+        let mut split_line=line.split_whitespace();
+        let next_word=split_line.next();
+        if next_word == Some(input){
+            let occurences:Vec<&str>=split_line.collect();
+            for occ in occurences.iter(){
+
+                let occ_int=occ.parse::<u64>().unwrap();
+                let start_pos=max(occ_int-margin,0);
+                let end_pos=min(occ_int+margin,korpus_size);
+
+                let reference=korpus_file.by_ref();
+                reference.seek(SeekFrom::Start(start_pos)).unwrap();
+                
+
+                let chunksize=end_pos-start_pos;
+
+                let mut buf=Vec::<u8>::with_capacity(chunksize as usize);
+                reference.take(chunksize).read_to_end(&mut buf);
+                let decoded=WINDOWS_1252.decode(&buf).0.trim().replace("\n", "|");
+               
+                println!("{}",decoded);
+                println!("----------------------------------");
+                
+            }
+            break;
+        }
+    }
+
+    println!("Hello, world!");
+}
+
+    fn calculate_index(chars: &mut Chars) -> usize {
+        let mut index = 0;
+        for i in (0..3).rev() {
+            let char_val = match chars.next() {
+                Some('ö') => 29,
+                Some('ä') => 28,
+                Some('å') => 27,
+                Some(n) => n as usize - 96,
+                None => 0,
+            };
+            index += 30usize.pow(i) * char_val
+        }
+        index
+    }
+    
